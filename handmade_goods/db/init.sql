95d14b2b85810df352424270128344e2f6e5e1e9
lhenon999 <77656081+lhenon999@users.noreply.github.com>
2025-02-27 17:59:29 +0900

    fix password hash

--- handmade_goods/db/init.sql
+++ handmade_goods/db/init.sql
@@ -10,10 +10,12 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+ALTER TABLE users CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 INSERT INTO users (name, email, password, user_type)
-SELECT 'administrator', 'admin@handmadegoods.com', '$2y$10$8BVxD8EpPDunffWh3ih2FeBpfjZxsHwLVW53eOZJISs.O5BlBByNK', 'admin'
+SELECT 'administrator', 'admin@handmadegoods.com', '$2y$10$E4LsPni7YFBS96DJ6tK8PeCJVgswuLXnd6XDPUySc3yCgbv6lnyeG', 'admin'
 WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'admin@handmadegoods.com');
 
 INSERT INTO users (name, email, password, user_type)
-SELECT 'John Doe', 'johndoe@mail.com', '$2y$10$tA.PReExstX08tJym/gF8u95ZAsBR8mvMIEBRTbAkquFQSyoyJDCq', 'normal'
+SELECT 'John Doe', 'johndoe@mail.com', '$2y$10$6FRuRI3lBFOpHtYhd29XIOs.sT7WeAYXWxs8ORhzLIvAqQApYchRu', 'normal'
 WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'johndoe@mail.com');
