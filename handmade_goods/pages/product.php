62958afda5400d74b352b8ab51685b811970aec2
lhenon999 <77656081+lhenon999@users.noreply.github.com>
2025-03-01 17:16:46 -0700

    edit listing button if listing is posted by logged in user

--- handmade_goods/pages/product.php
+++ handmade_goods/pages/product.php
@@ -37,13 +37,17 @@ $stmt->close();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $name ?> - Handmade Goods</title>
 
-    <style>@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&display=swap');</style>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&display=swap');
+    </style>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
@@ -52,6 +56,7 @@ $stmt->close();
     <link rel="stylesheet" href="../assets/css/footer.css">
     <link rel="stylesheet" href="../assets/css/product.css">
 </head>
+
 <body>
     <?php include '../assets/html/navbar.php'; ?>
 
@@ -66,18 +71,20 @@ $stmt->close();
             <p class="mt-4"><?= $description ?></p>
 
             <?php if (isset($_SESSION['user_id']) && $_SESSION['user_id'] == $product['user_id']): ?>
-    <a href="edit_product.php?id=<?= $product_id ?>" class="btn btn-primary">Edit Listing</a>
-<?php else: ?>
-    <form action="../basket/add_to_basket.php" method="POST">
-        <input type="hidden" name="product_id" value="<?= $product_id ?>">
-        <div class="quantity-add">
-            <input type="number" name="quantity" value="1" min="1" class="form-control">
-            <button type="submit" class="cta hover-raise atc">
-                <span class="material-symbols-outlined">add_shopping_cart</span> Add to Basket
-            </button>
-        </div>
-    </form>
-<?php endif; ?>
+                <a href="edit_listing.php?id=<?= $product['id'] ?>" class="cta hover-raise atc">
+        <span class="material-symbols-outlined">edit</span> Edit Listing
+    </a>
+            <?php else: ?>
+                <form action="../basket/add_to_basket.php" method="POST">
+                    <input type="hidden" name="product_id" value="<?= $product_id ?>">
+                    <div class="quantity-add">
+                        <input type="number" name="quantity" value="1" min="1" class="form-control">
+                        <button type="submit" class="cta hover-raise atc">
+                           c class="material-symbols-outlined">add_shopping_cart</> Add to Basket
+                        </button>
+                    </div>
+                </form>
+            <?php endif; ?>
 
 
             <a href="products.php" class="btn btn-outline-secondary mt-3">Back to Products</a>
@@ -86,4 +93,5 @@ $stmt->close();
 
     <?php include "../assets/html/footer.php"; ?>
 </body>
+
 </html>
