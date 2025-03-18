45fb8f222de366081c75de001b1fc517f867e5c8
RajveerSodhi <rajveersodhi03@gmail.com>
2025-03-18 01:21:10 -0700

    added custom 404 file

--- handmade_goods/index.php
+++ handmade_goods/index.php
@@ -3,9 +3,10 @@ session_start();
 $directory = ".";
 $files = scandir($directory);
 
+echo "<ul>";
 foreach ($files as $file) {
     if ($file != "." && $file != "..") {
         echo "<li><a href='$file'>$file</a></li>";
     }
 }
-echo "</ul>";
+echo "</ul>";f
---
+++ handmade_goods/pages/not_found.php
@@ -0,0 +1,35 @@
+<?php 
+session_start();
+include '../config.php';
+http_response_code(404);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Handmade Goods - Page Not Found</title>
+
+        <style>@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&display=swap');</style>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/css/globals.css">
+        <link rel="stylesheet" href="../assets/css/navbar.css">
+        <link rel="stylesheet" href="../assets/css/footer.css">
+        <link rel="stylesheet" href="../assets/css/home.css">
+    </head>
+
+    <body>
+        <?php include '../assets/html/navbar.php'; ?>
+
+        <div class="container text-center d-flex flex-column justify-content-center align-items-center mt-5">
+            <h1 class="mt-5">Uh Oh!</h1>
+            <p class="text-muted mb-5">The page you requested could not be found.</p>
+            <a href="./home.php" class="hover-raise cta mt-5">Go to Home</a>
+        </div>
+    </body>
+
+</html>
