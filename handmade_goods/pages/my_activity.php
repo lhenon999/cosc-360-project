6e3001729759306d5f3353f68147585b4a8233c2
lhenon999 <77656081+lhenon999@users.noreply.github.com>
2025-04-04 13:45:13 -0700

    fix cart bug

--- handmade_goods/pages/my_activity.php
+++ handmade_goods/pages/my_activity.php
@@ -1,7 +1,7 @@
 <?php
 include '../config.php';
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// error_reporting(E_ALL);
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
